b545735739b259d67b8c8ab4a425d4a4cb0973f9
TxHawks <tofu.hawks@gmail.com>
2016-03-28 22:03:35 +0300

    Remove unneeded dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,4 @@
 import browserSync from 'browser-sync';
-import del from 'del';
 
 import fs from 'fs';
 
@@ -12,12 +11,9 @@ import runSequence from 'run-sequence';
 
 // postCss plugins
 import autoprefixer from 'autoprefixer';
-import mdcss from 'mdcss';
 import reporter from 'postcss-reporter';
 import scssSyntax from 'postcss-scss';
 import stylelint from 'stylelint';
-
-import jigsassMdcss from 'mdcss-theme-jigsass';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
@@ -61,38 +57,6 @@ gulp.task('sass:doc', () => {
   }
 });
 
-gulp.task('sass:sg', ['sass:lint', 'sass:test', 'clean'], () => {
-  if (genDocs()) {
-    runSequence(
-      ['sass:lint', 'sass:test', 'clean'],
-      generateSg);
-  }
-
-  function generateSg() {
-    return gulp.src('sgSrc/sg.scss')
-      .pipe($.plumber())
-      .pipe($.sass.sync({
-        outputStyle: 'expanded',
-        precision: 10,
-        includePaths: ['scss'],
-      }).on('error', $.sass.logError))
-      .pipe($.postcss([
-        autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'ie > 7'] }),
-        mdcss({
-          theme: jigsassMdcss({ title: 'jigsass-generic-reset' }),
-          assets: ['sgSrc/assets'],
-        }),
-      ]));
-  }
-
-  function genDocs() {
-    try {
-      fs.accessSync('noStyleguide', fs.F_OK); return false;
-    }
-    catch (e) { return true; }
-  }
-});
-
 gulp.task('serve:sassdoc', ['sass:doc'], () => {
   browserSync({
     notify: true,
@@ -107,23 +71,6 @@ gulp.task('serve:sassdoc', ['sass:doc'], () => {
 
   gulp.watch(['scss/**/*.scss', 'DOCS.md'], ['sass:doc']);
 });
-
-gulp.task('serve:sg', ['sass:sg'], () => {
-  browserSync({
-    notify: true,
-    port: 9001,
-    reloadDelay: 4000,
-    server: {
-      baseDir: 'styleguide',
-    },
-  });
-
-  gulp.watch(['styleguide/**/*']).on('change', reload);
-
-  gulp.watch(['scss/**/*.scss', 'sgSrc/**/*', 'DOCS.md'], ['sass:sg']);
-});
-
-gulp.task('clean', del.bind(null, ['styleguide']));
 
 gulp.task('tdd', ['sass:lint', 'sass:test'], () => {
   gulp.watch('scss/**/*.scss', ['sass:lint', 'sass:test']);
